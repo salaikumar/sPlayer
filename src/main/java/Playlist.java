a5ed4264d694de62b3e84cafc47d0bceb45d33f4
Salaikumar <saravanan1115@gmail.com>
2016-01-04 09:21:59 +0530

    API Design for Playlist Class.

--- src/main/java/Playlist.java
+++ src/main/java/Playlist.java
@@ -8,4 +8,38 @@ public class Playlist {
 
     private String fileURI;
     private String[] songURI;
+    private String   name;      //Name of the Playlist
+
+    /*
+     * Not Sure If this Constructor is of any use
+     * Let it be there by now.
+     */
+    public Playlist(String fileURI, String name){
+        this.name = name;
+        this.fileURI = fileURI;
+
+//      Populate URI for each song in the playlist
+//      The Playlist is a simple text file.
+//      Each line corresponds to a song in the playlist.
+//      To be Done later
+    }
+
+    /*
+     * Create a Playlist by giving its name
+     */
+    public  Playlist(String name){
+        this.name = name;
+//     Create a file with the given name + timestamp at permitted location.
+    }
+
+
+    public boolean addSong(String songURI){
+        return false;
+    }
+
+    public boolean removeSong(Integer index){
+        return false;
+    }
+
+    
 }
